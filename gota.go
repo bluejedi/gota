522bcc90dd3023de5aa94a023928680056a8fbf1
bluejedi <jedi_biru@yahoo.co.uk>
2022-06-15 12:11:05 +0000

    add docker file and main

---
+++ gota.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	scanner "github.com/bluejedi/go-tvscanner"
+
+	"fmt"
+)
+
+
+func main() {
+	fmt.Println("Starting")
+	cl := scanner.New()
+	//data, _ := cl.GetAnalysis("indonesia", "IDX", "GOTO", "1d")
+	data, _ := cl.GetAnalysis("crypto", "BITFINEX", "BTCUSD", "1d")
+	//fmt.Println("result: ", data)
+	fmt.Printf("%+v\n", data)
+}
